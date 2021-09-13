1e2e9b292861fe0dbe2f0d7abf7dd08ba161dabe
dmalwaththa <67818142+dmalwaththa@users.noreply.github.com>
2021-09-13 12:06:27 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -11,5 +11,5 @@ async def start(client, message):
 
     ])
     thumbnail_url = "https://telegra.ph/file/35ba0a85f0e7f346e99ce.jpg"
-    await message.reply_photo(thumbnail_url, caption=f"**🙂 Hi <b>{message.from_user.first_name}**</b>\n\n<br>__😇 I Can Download YT Videos For You ✨️__</br>\n\n<b>• **🗂️ Instructions for use...🚨**</b>\n• **⚙ Type /help to get instructins...**\n \n───── ❝ **Lets Play** ❞ ─────\n ", reply_markup=Alpha)
+    await message.reply_photo(thumbnail_url, caption=f"**🙂 Hi <b>{message.from_user.first_name}**</b>\n\n<br>__😇 I Can Download YT Videos For You ✨️__</br>\n\n<b>• **🗂️ Instructions for use...🚨**</b>\n• **⚙ Type /help to get instructins...**\n \n───── ❝ **Lets Play by dila ** ❞ ─────\n ", reply_markup=Alpha)
     raise StopPropagation
